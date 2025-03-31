dc3f3f212a7bece443eaf6f18d3bb5a46a601fa1
Shashhank Seethula <shashhank.seethula@sjsu.edu>
2025-03-31 00:09:38 -0700

    connected DB, added static and template folders

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
     * pandas-2.2.3
     * scikit-learn-1.6.1
     * seaborn-0.13.2
-    * 
+    * matplotlib-3.10.1
+    * mysql-connector-python-9.2.0
+* MySQL 8.0.41
+    
+    
     
--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
+import mysql.connector
 app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="PASSWORD",
+    database="budget_buddy",
+    port="3306"
+)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    return str(users)
 
 app.run(debug=True)
